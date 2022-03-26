adbd61d1e53f06a5eb0641be022f6ef537caeb9f
alaminsahed <alaminsahed97@gmail.com>
2022-03-26 20:38:44 +0600

    chore: styling chat section

--- src/Components/Chat/Chat.js
+++ src/Components/Chat/Chat.js
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./Chat.css"
 const Chat = () => {
+    const [input, setInput] = useState(" ")
+    const sendMessage = (e) => {
+        e.preventDefault();
+    }
     return (
-        <div>
-            <h1>chat</h1>
+        <div className='chat'>
+            <div className="chat_header">
+
+                <h4>
+                    To:<span className='chat_name'>Channel Name</span>
+                </h4>
+                <strong>Details</strong>
+            </div>
+            <div className='chat_input'>
+                <form action="">
+                    <input value={input} type="text" placeholder='Message' onChange={(e) => setInput(e.target.value)} />
+                    <button onClick={sendMessage}>send</button>
+                </form>
+            </div>
         </div>
     );
 };
